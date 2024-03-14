0ea1b0a73e30b1e4ae80b6b060f42d5f2bb2a734
Golam Sarwar <gsshaun@gmail.com>
2024-03-14 20:02:23 +0900

    login sign_up routes

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,9 @@
+import { signUpRoute } from './signUpRoute';
+import { logInRoute } from './logInRoute';
 import { testRoute } from './testRoute';
 
 export const routes = [
+    logInRoute,
+    signUpRoute,
     testRoute,
 ];
