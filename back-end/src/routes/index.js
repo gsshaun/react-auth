58337d84da02c3d466015e1d77a41354c8fa1377
Golam Sarwar <gsshaun@gmail.com>
2024-03-16 01:03:28 +0900

    user update

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,9 +1,11 @@
 import { signUpRoute } from './signUpRoute';
 import { logInRoute } from './logInRoute';
+import { updateUserInfoRoute } from './updateUserInfoRoute';
 import { testRoute } from './testRoute';
 
 export const routes = [
     logInRoute,
     signUpRoute,
+    updateUserInfoRoute,
     testRoute,
 ];
--- front-end/src/pages/UserInfoPage.js
+++ front-end/src/pages/UserInfoPage.js
@@ -1,26 +1,24 @@
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useToken } from '../auth/useToken';
+import { useUser } from '../auth/useUser';
+import axios from 'axios';
 
 export const UserInfoPage = () => {
-    // We'll use the history to navigate the user
-    // programmatically later on (we're not using it yet)
+
+    const { user } = useUser();
+    const [token, setToken] = useToken();
     const history = useHistory();
 
-    // These states are bound to the values of the text inputs
-    // on the page (see JSX below). 
-    const [favoriteFood, setFavoriteFood] = useState('');
-    const [hairColor, setHairColor] = useState('');
-    const [bio, setBio] = useState('');
+    const { id, email, info } = user;
 
-    // These state variables control whether or not we show
-    // the success and error message sections after making
-    // a network request (see JSX below).
+    const [favoriteFood, setFavoriteFood] = useState(info?.favoriteFood || '');
+    const [hairColor, setHairColor] = useState(info?.hairColor || '');
+    const [bio, setBio] = useState(info?.bio || '');
+
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [showErrorMessage, setShowErrorMessage] = useState(false);
 
-    // This useEffect hook automatically hides the
-    // success and error messages after 3 seconds when they're shown.
-    // Just a little user interface improvement.
     useEffect(() => {
         if (showSuccessMessage || showErrorMessage) {
             setTimeout(() => {
@@ -31,28 +29,41 @@ export const UserInfoPage = () => {
     }, [showSuccessMessage, showErrorMessage]);
 
     const saveChanges = async () => {
-        // Send a request to the server to
-        // update the user's info with any changes we've
-        // made to the text input values
-        alert('Save functionality not implemented yet');
+        try {
+            const response = await axios.put(`/api/users/${id}`, {
+                hairColor,
+                favoriteFood,
+                bio
+            }, {
+                headers: { Authorization: `Bearer ${token}` }
+
+            });
+
+            const { token: newToken } = response.data;
+            setToken(newToken);
+            setShowSuccessMessage(true);
+        } catch (error) {
+            console.log(error);
+            setShowErrorMessage(true);
+        }
     }
 
     const logOut = () => {
-        // We'll want to log the user out here
-        // and send them to the "login page"
-        alert('Log out functionality not implemented yet');
+
+        localStorage.removeItem('token');
+        history.push('/login');
     }
-    
+
     const resetValues = () => {
-        // Reset the text input values to
-        // their starting values (the data we loaded from the server)
-        alert('Reset functionality not implemented yet');
+
+        setFavoriteFood(info?.favoriteFood);
+        setHairColor(info?.hairColor);
+        setBio(info?.bio);
     }
-    
-    // And here we have the JSX for our component. It's pretty straightforward
+
     return (
         <div className="content-container">
-            <h1>Info for ______</h1>
+            <h1>Info for {email}</h1>
             {showSuccessMessage && <div className="success">Successfully saved user data!</div>}
             {showErrorMessage && <div className="fail">Uh oh... something went wrong and we couldn't save changes</div>}
             <label>
